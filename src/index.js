603382cd5237331ba27a2f0fd1febb87764cc5a1
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-11-28 18:53:47 -0800

    Connect server and db with prisma bindings

--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-
-let idCount = links.length; 
+const { Prisma } = require("prisma-binding");
 
 //Define resolvers
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
+    }
   },
   Mutation: {
-      post: (root,args) => {
-          const link = {
-              id: `link-${idCount++}`,
-              description: args.description,
-              url: args.url,
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
           }
-          links.push(link)
-          return link
-      }
+        },
+        info
+      );
+    }
   }
 };
 
-//Bundle resolver and schema with GraphQLServer - tells which operations are accepted and how to be resolved
+//Bundle resolver,schema, and prisma with GraphQLServer - tells which operations are accepted and how to be resolved
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint: "https://us1.prisma.sh/kevinramosasuncion-d3881a/database/dev",
+      secret: "mysecret123",
+      debug: true
+    })
+  })
 });
 
 server.start(() => console.log(`Server running on http://localhost:4000`));
