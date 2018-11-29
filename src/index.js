12b5268962b98437a3aba5ce7171b534a9cda7ee
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-11-28 16:42:20 -0800

    add link type and feed resolver

--- src/index.js
+++ src/index.js
@@ -4,14 +4,30 @@ const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `;
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
 
 //Define resolvers
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
 };
 
 //Bundle resolver and schema with GraphQLServer - tells which operations are accepted and how to be resolved
